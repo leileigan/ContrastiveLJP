61e10e1e346f9331919218ae7de83105115418c9
ganleilei <leileigan@zju.edu.cn>
2022-01-21 10:43:02 +0800

    Add supervised contrastive learning model

---
+++ models/model_HARNN_Contra.py
@@ -0,0 +1,236 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2022/01/16 3:33 下午
+# @Author  : Leilei Gan
+# @Contact : 11921071@zju.edu.cn
+
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import datetime
+from utils.config import Config
+import random
+
+BERT_MODEL_PATH = "/mnt/data/ganleilei/chinese_L-12_H-768_A-12/"
+SEED_NUM = 2020
+torch.manual_seed(SEED_NUM)
+random.seed(SEED_NUM)
+np.random.seed(SEED_NUM)
+
+
+def SupConLoss(temperature=0.07, contrast_mode='all', features=None, labels=None, mask=None):
+    """Supervised Contrastive Learning: https://arxiv.org/pdf/2004.11362.pdf."""
+    device = (torch.device('cuda') if features.is_cuda else torch.device('cpu'))
+    """
+    features: [bsz, n_views, dim]
+
+    """
+    if len(features.shape) < 3:
+        raise ValueError('`features` needs to be [bsz, n_views, ...],'
+                         'at least 3 dimensions are required')
+    if len(features.shape) > 3:
+        features = features.view(features.shape[0], features.shape[1], -1)
+
+    batch_size = features.shape[0]
+    if labels is not None and mask is not None:
+        raise ValueError('Cannot define both `labels` and `mask`')
+    elif labels is None and mask is None:
+        mask = torch.eye(batch_size, dtype=torch.float32).to(device)
+    elif labels is not None:
+        labels = labels.contiguous().view(-1, 1)
+        if labels.shape[0] != batch_size:
+            raise ValueError('Num of labels does not match num of features')
+        mask = torch.eq(labels, labels.T).float().to(device)  # 1 indicates two items belong to same class
+    else:
+        mask = mask.float().to(device)
+
+    contrast_count = features.shape[1]  # num of views
+    contrast_feature = torch.cat(torch.unbind(features, dim=1), dim=0)  # (bsz * views, dim)
+    if contrast_mode == 'one':
+        anchor_feature = features[:, 0]
+        anchor_count = 1
+    elif contrast_mode == 'all':
+        anchor_feature = contrast_feature  # (bsz * views, dim)
+        anchor_count = contrast_count  # num of views
+    else:
+        raise ValueError('Unknown mode: {}'.format(contrast_mode))
+
+    # compute logits
+    anchor_dot_contrast = torch.div(
+        torch.matmul(anchor_feature, contrast_feature.T), temperature)  # (bsz, bsz)
+    # for numerical stability
+    logits_max, _ = torch.max(anchor_dot_contrast, dim=1, keepdim=True)  # (bsz, 1)
+    logits = anchor_dot_contrast - logits_max.detach()  # (bsz, bsz) set max_value in logits to zero
+    # logits = anchor_dot_contrast
+
+    # tile mask
+    mask = mask.repeat(anchor_count, contrast_count)  # (anchor_cnt * bsz, contrast_cnt * bsz)
+    # mask-out self-contrast cases
+    logits_mask = torch.scatter(torch.ones_like(mask), 1, torch.arange(batch_size * anchor_count).view(-1, 1).to(device),
+                                0)  # (anchor_cnt * bsz, contrast_cnt * bsz)
+    mask = mask * logits_mask  # 1 indicates two items belong to same class and mask-out itself
+
+    # compute log_prob
+    exp_logits = torch.exp(logits) * logits_mask  # (anchor_cnt * bsz, contrast_cnt * bsz)
+    log_prob = logits - torch.log(exp_logits.sum(1, keepdim=True) + 1e-12)
+
+    # compute mean of log-likelihood over positive
+    if 0 in mask.sum(1):
+        raise ValueError('Make sure there are at least two instances with the same class')
+    # temp = mask.sum(1)
+    mean_log_prob_pos = (mask * log_prob).sum(1) / (mask.sum(1) + 1e-12)
+
+    # loss
+    # loss = - (self.temperature / self.base_temperature) * mean_log_prob_pos
+    loss = -mean_log_prob_pos
+    loss = loss.view(anchor_count, batch_size).mean()
+    return loss
+
+
+class LawModel(nn.Module):
+
+    def __init__(self, config: Config):
+        super(LawModel, self).__init__()
+
+        self.word_dim = config.word_emb_dim
+        self.hidden_dim = config.HP_hidden_dim // 2
+        self.word_embeddings_layer = torch.nn.Embedding(config.pretrain_word_embedding.shape[0], config.pretrain_word_embedding.shape[1], padding_idx=0)
+
+        if config.pretrain_word_embedding is not None:
+            self.word_embeddings_layer.weight.data.copy_(torch.from_numpy(config.pretrain_word_embedding))
+            self.word_embeddings_layer.weight.requires_grad = False
+        else:
+            self.word_embeddings_layer.weight.data.copy_(
+                torch.from_numpy(self.random_embedding(config.word_alphabet_size, config.word_emb_dim)))
+
+        self.lstm_layer1 = nn.LSTM(self.word_dim, hidden_size=self.hidden_dim, num_layers=1, batch_first=True, bidirectional=True)
+        self.lstm_layer2 = nn.LSTM(self.hidden_dim * 2, hidden_size=self.hidden_dim, num_layers=1, batch_first=True, bidirectional=True)
+
+        self.attn_p = nn.Parameter(torch.Tensor(self.hidden_dim * 2, 1))
+        self.attn_q = nn.Parameter(torch.Tensor(self.hidden_dim * 2, 1))
+
+        nn.init.uniform_(self.attn_p, -0.1, 0.1)
+        nn.init.uniform_(self.attn_q, -0.1, 0.1)
+
+        self.accu_classifier = torch.nn.Sequential(
+            torch.nn.Dropout(0.3),
+            torch.nn.Linear(self.hidden_dim * 2, config.accu_label_size)
+        )
+        self.accu_loss = torch.nn.NLLLoss()
+        
+        self.law_classifier = torch.nn.Linear(self.hidden_dim * 2, config.law_label_size)
+        self.law_loss = torch.nn.NLLLoss()
+        
+        self.term_classifier = torch.nn.Linear(self.hidden_dim * 2, config.term_label_size)
+        self.term_loss = torch.nn.NLLLoss()
+
+        self.temperature = 0.7
+    
+
+    def classifier_layer(self, doc_out, accu_labels, law_labels, term_labels):
+        """
+        :param doc_out: [batch_size, 2 * hidden_dim]
+        :param accu_labels: [batch_size]
+        :param law_labels: [batch_size]
+        :param term_labels: [batch_size]
+        """
+        accu_logits = self.accu_classifier(doc_out)  # [batch_size, accu_label_size]
+        doc_out_dropout = doc_out.clone().detach()
+        accu_logits_dropout = self.accu_classifier(doc_out_dropout)
+        accu_probs = F.softmax(accu_logits, dim=-1)
+        accu_log_softmax = F.log_softmax(accu_logits, dim=-1)
+        accu_loss = self.accu_loss(accu_log_softmax, accu_labels)
+        _, accu_predicts = torch.max(accu_log_softmax, dim=-1) # [batch_size, accu_label_size]
+        
+        law_logits = self.law_classifier(doc_out)  # [batch_size, law_label_size]
+        law_probs = F.softmax(law_logits, dim=-1)
+        law_log_softmax = F.log_softmax(law_logits, dim=-1)
+        law_loss = self.law_loss(law_log_softmax, law_labels)
+        _, law_predicts = torch.max(law_log_softmax, dim=1) # [batch_size * max_claims_num]
+        
+        term_logits = self.term_classifier(doc_out)  # [batch_size, term_label_size]
+        term_probs = F.softmax(term_logits, dim=-1)
+        term_log_softmax = F.log_softmax(term_logits, dim=-1)
+        term_loss = self.term_loss(term_log_softmax, term_labels)
+        _, term_predicts = torch.max(term_log_softmax, dim=1) # [batch_size * max_claims_num]
+
+        if accu_labels is not None:
+            cl_loss = SupConLoss(temperature=self.temperature,
+                                 features=torch.stack([accu_logits, accu_logits_dropout], dim=1),
+                                 labels=accu_labels)
+
+        return accu_predicts, accu_loss, law_predicts, law_loss, term_predicts, term_loss, cl_loss
+
+
+
+
+    def neg_log_likelihood_loss(self, input_facts, accu_labels, law_labels, term_labels, input_sentences_lens, input_doc_len):
+        """
+        Args:
+            input_facts: [batch_size, max_sent_num, max_sent_seq_len]
+            input_laws: [law_num, max_law_seq_len]
+            law_labels: [batch_size]
+            accu_labels : [batch_size]
+            term_labels : [batch_size]
+            input_sentences_lens : int 
+            input_doc_len : int 
+
+        Returns:
+            [type]: [description]
+        """
+        batch_size, max_sent_num, max_sent_seq_len = input_facts.size()
+        doc_word_embeds = self.word_embeddings_layer.forward(input_facts.view(batch_size*max_sent_num, -1)) #[batch_size*max_sent_num, max_doc_seq_len, word_emb_size]
+
+        hidden = None
+        lstm1_out, hidden = self.lstm_layer1.forward(doc_word_embeds, hidden)
+        # [batch_size*max_sent_num, max_sent_seq_len, hidden_dim * 2]
+        attn_p_weights = torch.matmul(lstm1_out, self.attn_p)
+        attn_p_outs = F.softmax(attn_p_weights, dim=1)
+        doc_sent_rep = torch.sum(lstm1_out * attn_p_outs, dim=1).view(batch_size, max_sent_num, -1) #[batch_size, max_sent_num, hidden_dim*2]
+        
+        hidden = None
+        lstm2_out, hidden = self.lstm_layer2.forward(doc_sent_rep, hidden)
+        attn_q_weights = torch.matmul(lstm2_out, self.attn_q)
+        attn_q_out = F.softmax(attn_q_weights, dim=1)
+        doc_rep = torch.sum(lstm2_out*attn_q_out, dim=1) #[batch_size, hidden_dim * 2]
+
+        """
+        claim_word_embeds = self.word_embeddings_layer.forward(batch_claim_ids.view(batch_size * max_claims_num, -1))  # [batch_size*max_claims_num, max_claim_seq_len, word_emb_size]
+        # [batch_size, max_claim_num, max_sequence_len] -> [batch_size * max_claims_num, max_claim_seq_len, word_embed_dim]
+        hidden = None
+        claim_out, hidden = self.lstm_layer1.forward(claim_word_embeds, hidden)  # [batch_size * max_claims_num, max_claim_seq_len, hidden_dim * 2]
+        attn_p_weights = torch.matmul(claim_out, self.attn_p)
+        attn_p_outs = F.softmax(attn_p_weights, dim=1)
+        claim_rep = torch.sum(claim_out*attn_p_outs, dim=1).view(batch_size*max_claims_num, 1, -1)
+        doc_claim_rep = torch.cat((doc_sent_repeat_rep, claim_rep), dim=1) #[batch_size*max_claims_num, max_sen_num+1, hidden_dim*2]
+        hidden = None
+        lstm2_out, hidden = self.lstm_layer2.forward(doc_claim_rep, hidden)
+        attn_q_weights = torch.matmul(lstm2_out, self.attn_q)
+        attn_q_out = F.softmax(attn_q_weights, dim=1)
+        doc_rep = torch.sum(lstm2_out*attn_q_out, dim=1) #[batch_size, hidden_dim * 2]
+        """
+
+        accu_predicts, accu_loss, law_predicts, law_loss, term_predicts, term_loss, cl_loss = self.classifier_layer(doc_rep, accu_labels, law_labels, term_labels)  # [batch_size, 3]
+        return accu_loss, law_loss, term_loss, cl_loss, accu_predicts, law_predicts, term_predicts
+
+
+    def forward(self, input_x,  input_sentences_lens, input_fact, input_claims_y, input_claims_type):
+        #, input_sample_mask, input_sentences_mask
+
+        doc_rep = self.doc_encoder.forward(input_x, input_sentences_lens)  # [batch_size, hidden_dim]
+        claim_outputs = self.claim_classifier(doc_rep)  # [batch_size, 3]
+        claim_log_softmax = torch.nn.functional.log_softmax(claim_outputs, dim=1)
+        _, claim_predicts = torch.max(claim_log_softmax, dim=1)
+
+        fact_representation = self.fact_dense(doc_rep)  # [batch_size, hidden_dim] -> [batch_size, fact_num]
+        batch_size = fact_representation.size(0)
+        fact_logits = self.fact_classifier(self.fact_activation(fact_representation))
+        fact_predicts_prob = self.fact_sigmoid(fact_logits)
+        fact_predicts = torch.round(fact_predicts_prob)  # [batch_size, fact_num]
+
+        return fact_predicts, claim_predicts
+
+
+if __name__ == '__main__':
+   print(datetime.datetime.now())
---
+++ train_harnn_contrastive.py
@@ -0,0 +1,341 @@
+#-*- coding:utf-8 _*-
+# @Author: Leilei Gan
+# @Time: 2020/05/13
+# @Contact: 11921071@zju.edu.cn
+
+import argparse
+import copy
+import datetime
+import json
+import os
+import pickle
+import random
+import sys
+import time
+
+import numpy as np
+import torch
+from sklearn.metrics import (accuracy_score, classification_report, f1_score,
+                             precision_score, recall_score)
+from torch import optim
+from torch.utils.data.dataloader import DataLoader
+
+from models.model_HARNN_Contra import LawModel
+from utils.config import Config
+from utils.functions import load_data
+from utils.optim import ScheduledOptim
+from data.dataset import load_dataset, CustomDataset, collate_qa_fn
+
+from transformers import AutoTokenizer
+
+SEED_NUM = 2020
+torch.manual_seed(SEED_NUM)
+random.seed(SEED_NUM)
+np.random.seed(SEED_NUM)
+
+
+def load_model_decode(model_dir, config, dataset, name, gpu):
+    config.HP_gpu = gpu
+    print("Load Model from file: ", model_dir)
+    model = LawModel(config)
+    if config.HP_gpu:
+        model = model.cuda()
+    ## load model need consider if the model trained in GPU and load in CPU, or vice versa
+    # if not gpu:
+    #     model.load_state_dict(torch.load(model_dir), map_location=lambda storage, loc: storage)
+    #     # model = torch.load(model_dir, map_location=lambda storage, loc: storage)
+    # else:
+    model.load_state_dict(torch.load(model_dir))
+    # model = torch.load(model_dir)
+    score = evaluate(model, dataset, name)
+
+    return
+
+def str2bool(params):
+    return True if params.lower() == 'true' else False
+
+def save_data_setting(data: Config, save_file):
+    new_data = copy.deepcopy(data)
+    ## remove input instances
+    ## save data settings
+    with open(save_file, 'wb') as fp:
+        pickle.dump(new_data, fp)
+    print("Data setting saved to file: ", save_file)
+
+def lr_decay(optimizer, epoch, decay_rate, init_lr):
+    lr = init_lr * ((1 - decay_rate) ** epoch)
+    print(" Learning rate is setted as:", lr)
+    for param_group in optimizer.param_groups:
+        param_group['lr'] = lr
+    return optimizer
+
+
+def load_data_setting(save_file):
+    with open(save_file, 'rb') as fp:
+        data = pickle.load(fp)
+    print("Data setting loaded from file: ", save_file)
+    data.show_data_summary()
+    return data
+
+def get_result(accu_target, accu_preds, law_target, law_preds, term_target, term_preds, mode):
+    accu_macro_f1 = f1_score(accu_target, accu_preds, average="macro")
+    accu_macro_precision = precision_score(accu_target, accu_preds, average="macro")
+    accu_macro_recall = recall_score(accu_target, accu_preds, average="macro")
+
+    law_macro_f1 = f1_score(law_target, law_preds, average="macro")
+    law_macro_precision = precision_score(law_target, law_preds, average="macro")
+    law_macro_recall = recall_score(law_target, law_preds, average="macro")
+
+    term_macro_f1 = f1_score(term_target, term_preds, average="macro")
+    term_macro_precision = precision_score(term_target, term_preds, average="macro")
+    term_macro_recall = recall_score(term_target, term_preds, average="macro")
+
+    print("Accu task: macro_f1:%.4f, macro_precision:%.4f, macro_recall:%.4f" % (accu_macro_f1, accu_macro_precision, accu_macro_recall))
+    print("Law task: macro_f1:%.4f, macro_precision:%.4f, macro_recall:%.4f" % (law_macro_f1, law_macro_precision, law_macro_recall))
+    print("Term task: macro_f1:%.4f, macro_precision:%.4f, macro_recall:%.4f" % (term_macro_f1, term_macro_precision, term_macro_recall))
+
+    return (accu_macro_f1 + law_macro_f1 + term_macro_f1) / 3
+
+
+def data_initialization(data, train_file, dev_file, test_file):
+    print('begin building word alphabet set')
+    data.build_alphabet(train_file)
+    data.build_alphabet(dev_file)
+    data.build_alphabet(test_file)
+    data.fix_alphabet()
+    print('word alphabet size:', data.word_alphabet_size)
+    print('label alphabet size:', data.label_alphabet_size)
+    print('label alphabet:', data.label_alphabet.instances)
+    return data
+
+
+def evaluate(model, valid_dataloader, name):
+
+    model.eval()
+    ground_accu_y, ground_law_y, ground_term_y  = [], [], []
+    predicts_accu_y, predicts_law_y, predicts_term_y = [], [], []
+
+    for batch_idx, datapoint in enumerate(valid_dataloader):
+        fact_list, accu_label_lists, law_label_lists, term_lists = datapoint
+        _, _, _, _, accu_preds, law_preds, term_preds = model.neg_log_likelihood_loss(fact_list, accu_label_lists,law_label_lists, term_lists, config.sent_len, config.doc_len)
+
+        ground_accu_y.extend(accu_label_lists.tolist())
+        ground_law_y.extend(law_label_lists.tolist())
+        ground_term_y.extend(term_lists.tolist())
+
+        predicts_accu_y.extend(accu_preds.tolist())
+        predicts_law_y.extend(law_preds.tolist())
+        predicts_term_y.extend(term_preds.tolist())
+
+    accu_accuracy = accuracy_score(ground_accu_y, predicts_accu_y)
+    law_accuracy = accuracy_score(ground_law_y, predicts_law_y)
+    term_accuracy = accuracy_score(ground_term_y, predicts_term_y)
+    print("Accu task accuracy: %.4f, Law task accuracy: %.4f, Term task accuracy: %.4f" % (accu_accuracy, law_accuracy, term_accuracy)) 
+    score = get_result(ground_accu_y, predicts_accu_y, ground_law_y, predicts_law_y, ground_term_y, predicts_term_y, name)
+
+    return score
+
+
+def train(model, dataset, config: Config):
+    train_data_set = dataset["train_data_set"]
+    # train_data_set = dataset["valid_data_set"]
+    valid_data_set = dataset["valid_data_set"]
+    test_data_set = dataset["test_data_set"]
+    print("config batch size:", config.HP_batch_size)
+    train_dataloader = DataLoader(train_data_set, batch_size=config.HP_batch_size, shuffle=True, collate_fn=collate_qa_fn)
+    valid_dataloader = DataLoader(valid_data_set, batch_size=config.HP_batch_size, shuffle=False, collate_fn=collate_qa_fn)
+    test_dataloader = DataLoader(test_data_set, batch_size=config.HP_batch_size, shuffle=False, collate_fn=collate_qa_fn)
+
+    print("Training model...")
+    print(model)
+    parameters = filter(lambda p: p.requires_grad, model.parameters())
+
+    if config.use_warmup_adam:
+        optimizer = ScheduledOptim(optim.Adam(parameters, betas=(0.9, 0.98), eps=1e-9), d_model=256, n_warmup_steps=2000)
+    elif config.use_sgd:
+        optimizer = optim.SGD(parameters, lr=config.HP_lr, momentum=config.HP_momentum)
+    elif config.use_adam:
+        optimizer = optim.Adam(parameters, lr=config.HP_lr)
+    elif config.use_bert:
+        optimizer = optim.Adam(parameters, lr=5e-6)  # fine tuning
+    else:
+        raise ValueError("Unknown optimizer")
+    print('optimizer: ', optimizer)
+
+    best_dev = -1
+    no_imporv_epoch = 0
+    for idx in range(config.HP_iteration):
+        epoch_start = time.time()
+        temp_start = epoch_start
+        print("Epoch: %s/%s" % (idx, config.HP_iteration))
+        optimizer = lr_decay(optimizer, idx, config.HP_lr_decay, config.HP_lr)
+        sample_loss, sample_accu_loss, sample_law_loss, sample_term_loss, sample_cl_loss = 0, 0, 0, 0, 0
+
+        model.train()
+        model.zero_grad()
+
+        batch_size = config.HP_batch_size
+
+        ground_accu_y, predicts_accu_y = [], []
+        ground_law_y, predicts_law_y = [], []
+        ground_term_y, predicts_term_y = [], []
+
+        for batch_idx, datapoint in enumerate(train_dataloader):
+            fact_list, accu_label_lists, law_label_lists, term_lists = datapoint
+            accu_loss, law_loss, term_loss, cl_loss, accu_preds, law_preds, term_preds = model.neg_log_likelihood_loss(fact_list, 
+                                                                                                              accu_label_lists, 
+                                                                                                              law_label_lists, 
+                                                                                                              term_lists, 
+                                                                                                              config.sent_len, 
+                                                                                                              config.doc_len
+                                                                                                              )
+            loss = (accu_loss + term_loss + law_loss) / batch_size + 0.1 * cl_loss
+            sample_loss += loss.data
+            sample_accu_loss += accu_loss.data
+            sample_law_loss += law_loss.data
+            sample_term_loss += term_loss.data
+            sample_cl_loss += cl_loss.data
+
+            ground_accu_y.extend(accu_label_lists.tolist())
+            ground_law_y.extend(law_label_lists.tolist())
+            ground_term_y.extend(term_lists.tolist())
+
+            predicts_accu_y.extend(accu_preds.tolist())
+            predicts_law_y.extend(law_preds.tolist())
+            predicts_term_y.extend(term_preds.tolist())
+
+            cur_accu_accuracy = accuracy_score(ground_accu_y, predicts_accu_y)
+            cur_law_accuracy = accuracy_score(ground_law_y, predicts_law_y)
+            cur_term_accuracy = accuracy_score(ground_term_y, predicts_term_y)
+
+            if (batch_idx + 1 ) % 100 == 0:
+                temp_time = time.time()
+                temp_cost = temp_time - temp_start
+                temp_start = temp_time
+                print("Instance: %s; Time: %.2fs; loss: %.4f; accu loss %.4f; law loss %.4f; term loss %.4f; cl loss %.4f; accu acc %.4f; law acc %.4f; term acc %.4f" % 
+                ((batch_idx + 1), temp_cost, sample_loss, sample_accu_loss, sample_law_loss, sample_term_loss, sample_cl_loss, cur_accu_accuracy, cur_law_accuracy, cur_term_accuracy))
+                sys.stdout.flush()
+                sample_loss = 0
+                sample_accu_loss = 0
+                sample_law_loss = 0
+                sample_term_loss = 0
+                sample_cl_loss = 0
+
+            loss.backward()
+            # optimizer.step_and_update_lr()
+            optimizer.step()
+            model.zero_grad()
+
+        sys.stdout.flush()
+
+        # evaluate dev data
+        current_score = evaluate(model, valid_dataloader, "Dev")
+
+        if current_score > best_dev:
+            print("Exceed previous best acc score:", best_dev)
+            no_imporv_epoch = 0
+            best_dev = current_score
+            # save model
+            model_name = config.save_model_dir + '.' + str(idx) + ".model"
+            torch.save(model.state_dict(), model_name)
+            # evaluate test data
+            _ = evaluate(model, test_dataloader, "Test")
+        else:
+            no_imporv_epoch += 1
+            if no_imporv_epoch >= 10:
+                print("early stop")
+                break
+
+
+if __name__ == '__main__':
+    print(datetime.datetime.now())
+    parser = argparse.ArgumentParser(description='Contrastive Legal Judgement Prediction')
+    parser.add_argument('--data_path', default="/data/home/ganleilei/law/ContrastiveLJP/")
+    parser.add_argument('--status', default="train")
+    parser.add_argument('--savemodel', default="/data/home/ganleilei/law/ContrastiveLJP/models")
+    parser.add_argument('--savedset', default="/data/home/ganleilei/law/ContrastiveLJP/models")
+    parser.add_argument('--loadmodel', default="")
+
+    parser.add_argument('--embedding_path', default='/data/home/ganleilei/law/ContrastiveLJP/cail_thulac.npy')
+    parser.add_argument('--word_emb_dim', default=200)
+    parser.add_argument('--MAX_SENTENCE_LENGTH', default=250)
+    parser.add_argument('--hops', default=3)
+    parser.add_argument('--heads', default=4)
+    parser.add_argument('--max_decoder_step', default=100)
+
+    parser.add_argument('--HP_iteration', default=30)
+    parser.add_argument('--HP_batch_size', default=128)
+    parser.add_argument('--HP_hidden_dim', default=256)
+    parser.add_argument('--HP_dropout', default=0.2)
+    parser.add_argument('--HP_lstmdropout', default=0.5)
+    parser.add_argument('--HP_lstm_layer', default=1)
+    parser.add_argument('--HP_lr', default=1e-3, type=float)
+    parser.add_argument('--HP_lr_decay', default=0.05, type=float)
+
+    parser.add_argument('--use_warmup_adam', default='False')
+    parser.add_argument('--use_adam', default='True')
+
+    args = parser.parse_args()
+
+    status = args.status
+
+    if status == 'train':
+        if os.path.exists(args.loadmodel) and os.path.exists(args.savedset):
+            print('Load model path:', args.loadmodel)
+            print('Load save dataset:', args.savedset)
+            config: Config = load_data_setting(args.savedset)
+            model = LawModel(config)
+            model.load_state_dict(torch.load(args.loadmodel))
+        else:
+            print('New config....')
+            config = Config()
+            config.HP_iteration = args.HP_iteration
+            config.HP_batch_size = args.HP_batch_size
+            config.HP_hidden_dim = args.HP_hidden_dim
+            config.HP_dropout = args.HP_dropout
+            config.HP_lstm_layer = args.HP_lstm_layer
+            config.HP_lr = args.HP_lr
+            config.MAX_SENTENCE_LENGTH = args.MAX_SENTENCE_LENGTH
+            config.HP_lr_decay = args.HP_lr_decay
+            config.save_model_dir = args.savemodel
+            config.save_dset_dir = args.savedset
+            config.use_warmup_adam = str2bool(args.use_warmup_adam)
+            config.use_adam = str2bool(args.use_adam)
+            
+            config.load_word_pretrain_emb(args.embedding_path)
+            save_data_setting(config, config.save_dset_dir + '.dset')
+            model = LawModel(config)
+
+        config.show_data_summary()
+
+        if config.HP_gpu:
+            model.cuda()
+
+        print("\nLoading data...")
+        tokenizer_path = "/data/home/ganleilei/bert/bert-base-chinese/"
+        tokenizer = AutoTokenizer.from_pretrained(tokenizer_path)
+        train_data, valid_data, test_data = load_dataset(args.data_path)
+        train_dataset = CustomDataset(train_data, tokenizer, 512)
+        valid_dataset = CustomDataset(valid_data, tokenizer, 512)
+        test_dataset = CustomDataset(test_data, tokenizer, 512)
+
+        print("train_data %d, valid_data %d, test_data %d." % (
+            len(train_dataset), len(valid_dataset), len(test_dataset)))
+        data_dict = {
+            "train_data_set": train_dataset,
+            "test_data_set": test_dataset,
+            "valid_data_set": valid_dataset
+        }
+        
+        print("\nTraining...")
+        train(model, data_dict, config)
+
+    elif status == 'test':
+        if os.path.exists(args.loadmodel) is False or os.path.exists(args.savedset) is False:
+            print('File path does not exit: %s and %s' % (args.loadmodel, args.savedset))
+            exit(1)
+
+        config = load_data_setting(args.savedset)
+        print("\nLoading data...")
+        test_data = load_data(args.test, config)
+        decode_results = load_model_decode(args.loadmodel, config, test_data, 'Test', True)
