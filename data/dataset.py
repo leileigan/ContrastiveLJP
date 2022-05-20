f033e3625284d848ef9d454f54d419a4af4c6eae
ganleilei <leileigan@zju.edu.cn>
2022-05-20 13:24:16 +0000

    Add neurljudge custom dataset

--- data/dataset.py
+++ data/dataset.py
@@ -84,6 +84,53 @@ def collate_qa_fn(batch):
     return padded_fact_list, batch_raw_fact_list, padded_accu_label_lists, padded_law_label_lists, padded_term_lists
 
 
+class NeurJudgeDataset(Dataset):
+
+    def __init__(self, data, tokenizer, max_len, id2word_dict):
+        self.tokenizer = tokenizer
+        self.max_len = max_len
+        self.data = data
+        self.id2word_dict = id2word_dict
+
+    def __len__(self):
+        return len(self.data['fact_list'])
+
+    def _convert_ids_to_sent(self, fact):
+        #fact: [max_sent_len, ]
+        mask = np.array(fact) == 164672
+        mask = ~mask
+        seq_len = mask.sum(0)
+        return [self.id2word_dict[id] for id in fact[:seq_len]]
+    
+
+    def __getitem__(self, index):
+        fact_list = self.data['fact_list'][index]
+        raw_fact_list = self._convert_ids_to_sent(fact_list) 
+        accu_label_lists = self.data['accu_label_lists'][index]
+        law_label_lists = self.data['law_label_lists'][index]
+        term_lists = self.data['term_lists'][index]
+        
+        return fact_list, raw_fact_list, accu_label_lists, law_label_lists, term_lists 
+
+
+def collate_neur_judge_fn(batch):
+    
+    batch_fact_list, batch_raw_fact_list, batch_law_label_lists, batch_accu_label_lists, batch_term_lists = [], [], [], [], []
+    for item in batch:
+        batch_fact_list.append(item[0])
+        batch_raw_fact_list.append(item[1])
+        batch_accu_label_lists.append(item[2])
+        batch_law_label_lists.append(item[3])
+        batch_term_lists.append(item[4])
+
+    padded_fact_list = torch.LongTensor(batch_fact_list).to(DEVICE)
+    padded_accu_label_lists = torch.LongTensor(batch_accu_label_lists).to(DEVICE)
+    padded_law_label_lists = torch.LongTensor(batch_law_label_lists).to(DEVICE)
+    padded_term_lists = torch.LongTensor(batch_term_lists).to(DEVICE)
+
+    return padded_fact_list, batch_raw_fact_list, padded_accu_label_lists, padded_law_label_lists, padded_term_lists
+
+
 if __name__ == "__main__":
     data_path = "/data/home/ganleilei/law/ContrastiveLJP/"
     tokenizer_path = "/data/home/ganleilei/bert/bert-base-chinese/"
